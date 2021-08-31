206f433789edaed6aa73b6b35d2bf72a0a08675c
Nadia Naji <nadialnaji@gmail.com>
2021-08-31 16:45:57 -0600

    Moved images from data to assets folder

--- src/scripts.js
+++ src/scripts.js
@@ -1,7 +1,12 @@
 import './styles.css';
 import apiCalls from './apiCalls';
-
-
+import './assets/burger.png';
+// import burger.png from './assets'
+import './assets/favorite.svg';
+import './assets/fridge.svg';
+import './assets/home.svg';
+import './assets/list.svg';
+import './assets/search.svg';
 
 
 console.log('Hello world');
