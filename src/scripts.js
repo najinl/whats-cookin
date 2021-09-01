1205df7e49f9ad0f7ad126a185c938de466533fb
Nadia Naji <nadialnaji@gmail.com>
2021-09-01 14:57:14 -0600

    Add event listeners and recipe card mods

--- src/scripts.js
+++ src/scripts.js
@@ -8,5 +8,48 @@ import './assets/home.svg';
 import './assets/list.svg';
 import './assets/search.svg';
 
-
 console.log('Hello world');
+
+
+
+let homeNavBtn = document.getElementById('homeNav');
+let favNavBtn = document.getElementById('favNav');
+let listNavBtn = document.getElementById('listNav');
+let recipeCard = document.getElementById('recipeCard');
+
+window.addEventListener('load', (e) => {
+  addClass(homeNavBtn, 'hidden');
+})
+
+homeNavBtn.addEventListener('click', function() {
+  addClass(homeNavBtn, 'hidden');
+  removeClass(favNavBtn, 'hidden');
+  removeClass(listNavBtn, 'hidden');
+})
+
+favNavBtn.addEventListener('click', function() {
+  addClass(favNavBtn, 'hidden');
+  removeClass(homeNavBtn, 'hidden');
+  removeClass(listNavBtn, 'hidden');
+})
+
+listNavBtn.addEventListener('click', function() {
+  addClass(listNavBtn, 'hidden');
+  removeClass(homeNavBtn, 'hidden')
+  removeClass(favNavBtn, 'hidden')
+})
+
+recipeContainer.addEventListener('click', function(e) {
+  if(e.target.classList.contains('like-btn')) {
+    e.target.classList.add('unlilke-btn');
+    e.target.classList.remove('like-btn');
+  }
+})
+
+const addClass = (element, classList) => {
+  element.classList.add(classList);
+}
+
+const removeClass = (element, classList) => {
+  element.classList.remove(classList);
+}
