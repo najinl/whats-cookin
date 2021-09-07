0e3b984a2ab024f1e8fc2818e314a161b3a260bb
Janika Hortizuela <jhortizuela21@gmail.com>
2021-09-07 15:10:45 -0600

    Organize code

--- src/scripts.js
+++ src/scripts.js
@@ -11,12 +11,23 @@ import Recipe from './classes/Recipe.js';
 import RecipeRepository from './classes/RecipeRepository';
 import User from './classes/User';
 
+//global variables, instantiations of classes
+let ingredients, recipeRepository, recipe, user, displayRecipe, modalContainer, closeModalBtn, favoritedRecipes, likedRecipes,
+foundRecipeIngredients, foundRecipeNames
 
-//global variables, instantiations of classes, holds info
-let ingredients, recipeRepository, recipe, user, displayRecipe, modalContainer, closeModalBtn, favoritedRecipes, likedRecipes
+let homeNavBtn = document.getElementById('homeNav');
+let favNavBtn = document.getElementById('favNav');
+let listNavBtn = document.getElementById('listNav');
+var recipeLikeBtns = document.getElementsByClassName('btn');
+let recipeContainer = document.getElementById('recipeContainer');
+let filterBtn = document.getElementById('filterBtn');
+let searchBtn = document.getElementById('searchIcon');
+let searchValues = document.getElementById('inputSearch');
+let favoritedContainer = document.getElementById('favoritedRecipeContainer');
+let myListContainer = document.getElementById('listRecipeContainer');
+let welcomename = document.getElementById('username')
 
-
-
+//Fetch Calls
 const fetchData = () => {
   Promise.all([fetchUsers(), fetchIngredients(), fetchRecipes()])
     .then(data => parseData(data))
@@ -31,7 +42,6 @@ const parseData = (data) => {
 }
 
 const instantiation = (userDataArray, ingredientDataArray, recipeDataArray) => {
-  //math random for random user, do not have user class yet
   let i = Math.floor(Math.random() * userDataArray.length);
   ingredients = new IngredientsLibrary(ingredientDataArray)
   recipeRepository = new RecipeRepository(recipeDataArray)
@@ -39,71 +49,29 @@ const instantiation = (userDataArray, ingredientDataArray, recipeDataArray) => {
     return new Recipe(recipe, ingredientDataArray)
   })
   user = new User(userDataArray[i], recipeRepository);
+  console.log(user.name)
 }
 
-
-const renderinfo = () => {
-  // console.log(recipe[0].image)
-  // console.log(ingredients)
-  showCards(recipe)
-}
-
-let homeNavBtn = document.getElementById('homeNav');
-let favNavBtn = document.getElementById('favNav');
-let listNavBtn = document.getElementById('listNav');
-var recipeLikeBtns = document.getElementsByClassName('btn');
-let recipeContainer = document.getElementById('recipeContainer');
-let lunch = document.getElementById('lunch')
-let filterBtn = document.getElementById('filterBtn')
-let searchBtn = document.getElementById('searchIcon')
-let searchValues = document.getElementById('inputSearch')
-
-filterBtn.addEventListener('click', (event) => {
-  getTags('check')
+//Event Listeners
+window.addEventListener('load', (e) => {
+  addClass(homeNavBtn, 'hidden');
+  fetchData()
+  renderName()
 })
 
 searchBtn.addEventListener('click', (event) => {
   getSearchItems()
 })
 
-function getTags(name) {
-  let checkBox = document.querySelectorAll(`input[name="${name}"]:checked`)
-  let findChecks = document.querySelectorAll(`input[name="${name}"]:checked`).length
-  let checkedElements = [];
-  checkBox.forEach((checkbox) => {
-    checkedElements.push(checkbox.value)
-  }) 
-  let newRecipes = recipeRepository.filterByTags(checkedElements)
-
-  if(findChecks === 0) {
-    showCards(recipe)
-  } else {
-    clearCards()
-    showCards(newRecipes)
-  }
-}
-
-function getSearchItems() {
-let search = searchValues.value
-let foundRecipes = recipeRepository.filterByIngredients(search, ingredients.ingredientsLibrary)
-
-
- clearCards()
-showCards(foundRecipes)
-
-}
-
-
-let favoritedContainer = document.getElementById('favoritedRecipeContainer');
-let myListContainer = document.getElementById('listRecipeContainer');
-
-window.addEventListener('load', (e) => {
-  addClass(homeNavBtn, 'hidden');
-  fetchData()
-  console.log(recipe)
+filterBtn.addEventListener('click', (event) => {
+  if(searchValues.value.length> 0) {
+    getTags()
+   
+  } 
+  console.log(searchValues.value)
 })
 
-homeNavBtn.addEventListener('click', function() {
+homeNavBtn.addEventListener('click', () => {
   favoritedContainer.innerHTML = '';
   myListContainer.innerHTML = '';
   addClass(homeNavBtn, 'hidden');
@@ -115,7 +83,7 @@ homeNavBtn.addEventListener('click', function() {
   refreshLiked()
 })
 
-favNavBtn.addEventListener('click', function() {
+favNavBtn.addEventListener('click', () => {
   favoritedContainer.innerHTML = '';
   myListContainer.innerHTML = '';
   addClass(favNavBtn, 'hidden');
@@ -124,14 +92,14 @@ favNavBtn.addEventListener('click', function() {
   showFavorites();
 })
 
-listNavBtn.addEventListener('click', function() {
+listNavBtn.addEventListener('click', () => {
   addClass(listNavBtn, 'hidden');
   removeClass(homeNavBtn, 'hidden')
   removeClass(favNavBtn, 'hidden')
   showMyList()
 });
 
-recipeContainer.addEventListener('click', function(e) {
+recipeContainer.addEventListener('click', (e) => {
   let targetBtn = e.target;
   let recipeID = parseInt(targetBtn.closest('.recipe-card').id);
   favoritedRecipes = user.myFavorites;
@@ -158,14 +126,11 @@ recipeContainer.addEventListener('click', function(e) {
       modalContainer.classList.add('show')
     }
   }
-  console.log('Home List:',user.myList);
-  // console.log('Home Fav:',user.myFavorites);
 })
 
-favoritedContainer.addEventListener('click', function(e) {
+favoritedContainer.addEventListener('click', (e) => {
   let targetBtn = e.target;
   let recipeID = parseInt(targetBtn.closest('.favorited-recipe-card').id);
-  console.log('recipeID on fav,', recipeID)
 
   if(targetBtn.classList.contains('unfavorite')) {
       removeFromFavorites(targetBtn, favoritedRecipes, recipeID);
@@ -179,38 +144,28 @@ favoritedContainer.addEventListener('click', function(e) {
     else if(targetBtn.classList.contains('remove') && !targetBtn.classList.contains('add')){
       removeFromCookList(targetBtn, likedRecipes, recipeID);
     }
-    console.log('Fav List:',user.myList);
-    // console.log('Fav Fav:',user.myFavorites);
-  // console.log(user.myList);
 })
 
-myListContainer.addEventListener('click', function(e) {
+myListContainer.addEventListener('click', (e) => {
   let targetBtn = e.target;
   let recipeID = parseInt(targetBtn.closest('.list-recipe-card').id);
-  console.log(recipeID);
+
   if(targetBtn.classList.contains('unfavorite') && targetBtn.classList.contains('favorite')) {
       addToFavorites(targetBtn, favoritedRecipes, recipeID);
-      console.log('screw')
     }
     else if(targetBtn.classList.contains('unfavorite') && !targetBtn.classList.contains('favorite')) {
       removeFromFavorites(targetBtn, favoritedRecipes, recipeID);
-      console.log('this')
     }
     else if(targetBtn.classList.contains('add') && targetBtn.classList.contains('remove')) {
       addToCookList(targetBtn, recipeID);
-      console.log('shit')
     }
     else if(targetBtn.classList.contains('remove')){
       removeFromCookList(targetBtn, likedRecipes, recipeID);
-      console.log('man')
     }
-    console.log('List List:',user.myList);
-    // console.log('List Fav:',user.myFavorites);
 })
 
-function refreshFavorited() {
+const refreshFavorited = () => {
 let recipeCard = recipeContainer.querySelectorAll('.recipe-card');
-// let favBtn = document.querySelector('.btn unfavorite');
   recipeCard.forEach(recipe => {
     let recipeActions = recipe.getElementsByClassName('recipe-actions');
     let favBtn = recipeActions[0].children[2]
@@ -223,7 +178,7 @@ let recipeCard = recipeContainer.querySelectorAll('.recipe-card');
   })
 }
 
-function refreshLiked() {
+const refreshLiked = () => {
 let recipeCard = recipeContainer.querySelectorAll('.recipe-card');
   recipeCard.forEach(recipe => {
     let recipeActions = recipe.getElementsByClassName('recipe-actions');
@@ -237,8 +192,8 @@ let recipeCard = recipeContainer.querySelectorAll('.recipe-card');
   })
 }
 
-
-function showCards(data) {
+//Functions
+const showCards = (data) => {
   data.map(oneRecipe =>
     {
     const recipecard = document.createElement('div');
@@ -258,15 +213,14 @@ function showCards(data) {
   })
 };
 
-function clearCards() {
+const clearCards = () => {
     let removeElement = document.getElementById('recipeContainer')
     while (removeElement.firstChild) {
       removeElement.removeChild(removeElement.firstChild);
     }
   }
 
-function showFavorites() {
-  // console.log('FAVORITES', favoritedRecipes)
+const showFavorites = () => {
   recipe.forEach(currRecipe =>
     {
     const favoritedRecipeCard = document.createElement('div');
@@ -288,8 +242,7 @@ function showFavorites() {
   removeClass(favoritedContainer, 'hidden');
 };
 
-function showMyList() {
-  // console.log('FAVORITES', favoritedRecipes)
+const showMyList = () => {
   recipe.forEach(currRecipe =>
     {
     const listRecipeCard = document.createElement('div');
@@ -311,51 +264,39 @@ function showMyList() {
   addClass(favoritedContainer, 'hidden');
 };
 
-function addToFavorites(targetBtn, id) {
+const addToFavorites = (targetBtn, id) => {
   targetBtn.classList.remove('favorite')
   user.favoriteRecipes(id);
 };
 
-function removeFromFavorites(targetBtn, favoritedRecipes, id) {
+const removeFromFavorites = (targetBtn, favoritedRecipes, id) => {
   targetBtn.classList.add('favorite')
   user.unfavoriteRecipes(favoritedRecipes, id);
 }
 
-function addToCookList(targetBtn, id) {
+const addToCookList = (targetBtn, id) => {
   targetBtn.classList.remove('add')
   user.addToMyList(id);
 }
 
-function removeFromCookList(targetBtn, likedRecipes, id) {
+const removeFromCookList = (targetBtn, likedRecipes, id) => {
   targetBtn.classList.add('add')
   user.removeFromMyList(likedRecipes, id)
 }
 
-function makeModal(recipe) {
+const makeModal = (recipe) => {
   const newModal = document.createElement('div');
   let checkModal = document.getElementById('modalContainer');
   if(checkModal !== null) {
     recipeContainer.removeChild(checkModal);
   }
-
-  // let instructions = recipe.instructions(instruction => {
-  //   return `${instruction.number}. ${instruction}`
-  // })
   let howTo = recipe.instructions.map(instruction => {
     return `${instruction.number}. ${instruction.instruction} <br><br>`
   })
-
   let gatheredIngredients = recipe.gatherIngredients()
-console.log(gatheredIngredients)
-  
-let styleIngredientsList = gatheredIngredients.map(ingredient => {
+  let styleIngredientsList = gatheredIngredients.map(ingredient => {
     return `${ingredient}<br>`
   })
-
-  console.log(styleIngredientsList)
-
-  // we will need to modify the instructions with an iterator to get all of them included, I just did this for sake of time tonight. (line 193<p>) We need to thing out how we're going to number them too. I was thinking in front like this 1) preheat the oven...
-  // I am thinking reduce() with an accumulator that concatenates the instructions into a single string
   let modal =
     `<div class="modal-container" id="modalContainer">
       <div class="modal" id="modal">
@@ -378,13 +319,47 @@ let styleIngredientsList = gatheredIngredients.map(ingredient => {
     modalContainer.classList.add('show');
     closeModalBtn.addEventListener('click', function() {
     modalContainer.classList.remove('show');
-    });
+  });
 }
 
-function addClass(element, classList) {
+const addClass = (element, classList) => {
   element.classList.add(classList);
 }
 
-function removeClass (element, classList) {
+const removeClass = (element, classList) => {
   element.classList.remove(classList);
+}
+
+const getSearchItems = () => {
+  let search = searchValues.value
+  foundRecipeIngredients = recipeRepository.filterByIngredients(search, ingredients.ingredientsLibrary) 
+  foundRecipeNames = recipeRepository.filterByName(search)
+   clearCards()
+   showCards(foundRecipeNames)
+   showCards(foundRecipeIngredients)
+  }
+
+const getTags = ()  => {
+  let checkBox = document.querySelectorAll(`input[name="check"]:checked`)
+  let findChecks = document.querySelectorAll(`input[name="check"]:checked`).length
+  let checkedElements = [];
+  checkBox.forEach((checkbox) => {
+    checkedElements.push(checkbox.value)
+  }) 
+  let newRecipes = recipeRepository.filterByTags(checkedElements)
+  if(findChecks === 0) {
+    showCards(recipe)
+  } else {
+    clearCards()
+    showCards(newRecipes)
+  }
+}
+
+const renderinfo = () => {
+  showCards(recipe)
+}
+
+const renderName = (user) => {
+  const renderedText = `What's cookin ${user.name}?`;
+  welcomename.innerText += renderedText
 }
