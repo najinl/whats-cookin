6c3a3c5a9eaa984ce1e830a099d8d82148e99cb4
Nadia Naji <nadialnaji@gmail.com>
2021-09-07 20:54:33 -0600

    Remove comments

--- src/classes/User.js
+++ src/classes/User.js
@@ -6,23 +6,7 @@ class User {
     this.name = user.name;
     this.myFavorites = [];
     this.myList = [];
-    // this.recipeOptions = new RecipeOptions(recipeData, ingredientsData);
   }
-
-// buildUserData() {
-//   this.pantry.makeIngredients();
-// }
-
-checkIfCookable() {
-  //iterate over the myList array and match id to id within the recipeRopository
-  //pull out the ingredients and amount of ingredients
-  //do you have enough ingredients to cook the recipe
-    //check the ingredient name is included ... i'm thinking .contains here?
-    //if all ingredients are contained, then check amounts to see if you have the right amounts of each ingredients
-
-    //if true, return "Let's get cookin'!"
-    //else, you need "2 tsps of salt"
-}
 
 unfavoriteRecipes(arrToFavorites, id) {
   arrToFavorites.forEach((favorite, i) => {
