3ea9de0298403fea519def47a87036f996789a49
Nadia Naji <nadialnaji@gmail.com>
2021-09-16 09:07:57 -0600

    Create Pantry class

---
+++ src/classes/Pantry.js
@@ -0,0 +1,65 @@
+import IngredientsLibrary from './ingredientsRepository';
+
+class Pantry {
+  constructor(pantry, ingredientsData) {
+    this.rawPantryData = pantry;
+    this.ingredients = [];
+    this.ingredientsById = [];
+    this.ingredientsNeeded = [];
+    this.ingredientInventory = new IngredientsLibrary(ingredientsData);
+  }
+
+  addIngredientsByName() {
+    let findIngredients = this.ingredientInventory.ingredientsLibrary.forEach(ingredient => {
+      this.rawPantryData.forEach(pantryIngredient => {
+       if(!this.ingredientsById.includes(pantryIngredient.ingredient))
+        this.ingredientsById.push(pantryIngredient.ingredient)
+       if(pantryIngredient.ingredient === ingredient.id) {
+         pantryIngredient.name = ingredient.name;
+         pantryIngredient.estimatedCostInCents = ingredient.estimatedCostInCents;
+         this.ingredients.push(pantryIngredient)
+       }
+      })
+    })
+  }
+
+determineAmtNeeded(recipe) {
+let neededIngredients = recipe.ingredients.reduce((acc, ingredient) => {
+  this.ingredients.forEach(ingr => {
+    if(ingredient.id === ingr.ingredient && ingredient.quantity.amount > ingr.amount) {
+      acc.push({name: ingr.name,
+       amountNeeded: ingredient.quantity.amount - ingr.amount})
+    }
+  })
+return acc;
+},[])
+
+recipe.ingredients.forEach(ingredient => {
+    if(!this.ingredientsById.includes(ingredient.id)) {
+      neededIngredients.push({name: ingredient.id, amountNeeded: ingredient.quantity.amount})
+    }
+})
+
+neededIngredients.forEach(ingredient => {
+  this.ingredientInventory.ingredientsLibrary.forEach(ingr => {
+  if(ingredient.name === ingr.id) {
+    ingredient.name = ingr.name
+  }
+  })
+})
+this.ingredientsNeeded = neededIngredients;
+// console.log('length',this.ingredientsNeeded.length)
+}
+
+returnCookMessage() {
+  let returnMsg =this.ingredientsNeeded.
+    reduce((acc, needed) => {
+    acc.push(`You need ${needed.amountNeeded} more ${needed.name}.`)
+    return acc;
+    }, [])
+return returnMsg.join(' ')
+}
+}
+
+
+export default Pantry;
