5d2659935376fc12485514a70b72f1bb67a8cff4
Nadia Naji <nadialnaji@gmail.com>
2021-09-16 18:29:35 -0600

    Add Pantry instantiation to Users class

--- src/classes/User.js
+++ src/classes/User.js
@@ -1,36 +1,37 @@
-// import Pantry from './Pantry';
-// import RecipeOptions from './RecipeOptions';
+import Pantry from './Pantry';
+
 class User {
-  constructor(user, recipeRepository) {
+  constructor(user, recipeRepository, ingredientsData) {
     this.id = user.id;
     this.name = user.name;
     this.myFavorites = [];
     this.myList = [];
+    this.myPantry = new Pantry(user.pantry, ingredientsData);
   }
 
-unfavoriteRecipes(arrToFavorites, id) {
-  arrToFavorites.forEach((favorite, i) => {
-    if(favorite === parseInt(id)) {
-      return arrToFavorites.splice(i, 1)
-    }
-  })
-}
+  unfavoriteRecipes(arrToFavorites, id) {
+    arrToFavorites.forEach((favorite, i) => {
+      if(favorite === parseInt(id)) {
+        return arrToFavorites.splice(i, 1)
+      }
+    })
+  }
 
-favoriteRecipes(id) {
-  this.myFavorites.push(parseInt(id));
-}
+  favoriteRecipes(id) {
+    this.myFavorites.push(parseInt(id));
+  }
 
-addToMyList(id){
-  this.myList.push(parseInt(id));
-}
+  addToMyList(id){
+    this.myList.push(parseInt(id));
+  }
 
-removeFromMyList(arrToList, id){
-  arrToList.forEach((item, i) => {
-    if(item === parseInt(id)) {
-      return arrToList.splice(i, 1)
-    }
-  })
-}
+  removeFromMyList(arrToList, id){
+    arrToList.forEach((item, i) => {
+      if(item === parseInt(id)) {
+        return arrToList.splice(i, 1)
+      }
+    })
+  }
 }
 
 export default User;
